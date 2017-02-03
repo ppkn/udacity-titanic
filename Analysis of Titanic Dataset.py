c19e66984f985d8dce07ba6e82c14bae34197c43
Daniel Pipkin <drpipkin@gmail.com>
2017-02-02 17:27:12 -0700

    :sparkles: Start presentation notebook

---
+++ Analysis of Titanic Dataset.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# # Analysis of Titanic Dataset
+# Author: Daniel Pipkin
+
+# In[3]:
+
+get_ipython().magic('matplotlib inline')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('seaborn-pastel')
+
